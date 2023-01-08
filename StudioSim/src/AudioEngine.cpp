1f5944153e32011c583136ae35027563457d6c1d
Fernandes <71331946+Felp0@users.noreply.github.com>
2023-01-08 00:14:44 +0000

    Prototype based on website working

--- StudioSim/src/AudioEngine.cpp
+++ StudioSim/src/AudioEngine.cpp
@@ -1,5 +1,8 @@
 #include "AudioEngine.h"
 
+/// <summary>
+/// Function initializing Fmod
+/// </summary>
 Init::Init()
 {
 	pSystem = nullptr;
@@ -22,5 +25,258 @@ Init::Init()
 		exit(-1);
 	}
 
+}
+
+/// <summary>
+/// Checking if a channel stopped playing, if yes, destroy and clear a new channel
+/// </summary>
+void Init::InitUpdate()
+{
+	vector<ChannelMap::iterator> channelsThatStooped;
+
+	for (auto i = m_Channels.begin(), iEnd = m_Channels.end(); i != iEnd; ++i)
+	{
+		bool playing = false;
+		i->second->isPlaying(&playing);
+		if (!playing)
+		{
+			channelsThatStooped.push_back(i);
+		}
+	}
+	for (auto& i : channelsThatStooped)
+	{
+		m_Channels.erase(i);
+	}
+	
+	pSystem->update();
 
 }
+
+Init* pInit = nullptr;
+
+//Start of the creation of the audio engine
+
+/// <summary>
+/// Initializing Audio Engine;
+/// </summary>
+void AudioEngine::InitializingAudioEngine()
+{
+	pInit = new Init;
+}
+
+/// <summary>
+/// Audio Engine Update
+/// </summary>
+void AudioEngine::UpdateAudioEngine()
+{
+	pInit->InitUpdate();
+}
+
+/// <summary>
+/// loading Sound
+/// </summary>
+/// <param name="soundName"></param>
+/// <param name="looping"></param>
+/// <param name="is2D"></param>
+void AudioEngine::LoadSound(const std::string& soundName, bool looping, bool is2D)
+{
+	auto foundSound = pInit->m_Sounds.find(soundName);
+
+	///Sound description bitfields, bitwise OR them together for loading and describing sounds.
+	FMOD_MODE fmodMode = FMOD_DEFAULT;
+	fmodMode |= looping ? FMOD_LOOP_NORMAL : FMOD_LOOP_OFF;
+	fmodMode |= is2D ? FMOD_2D : FMOD_3D;
+	FMOD::Sound* pSound = nullptr;
+	
+	pInit->pSystem->createSound(soundName.c_str(), fmodMode, nullptr, &pSound);
+	if (pSound)
+	{
+		pInit->m_Sounds[soundName] = pSound;
+	}
+}
+
+/// <summary>
+/// Unloading Sound
+/// Unloading to free memory.
+/// </summary>
+/// <param name="soundName"></param>
+void AudioEngine::UnloadSound(const std::string& soundName)
+{
+	auto foundSound = pInit->m_Sounds.find(soundName);
+
+	if (foundSound == pInit->m_Sounds.end())
+	{
+		return;
+	}
+
+	foundSound->second->release();
+	pInit->m_Sounds.erase(foundSound);
+
+}
+
+
+/// <summary>
+/// Playing Sound
+/// Checks if the sound is in the map, if not Load
+/// </summary>
+/// <param name="soundName"></param>
+/// <param name="pos"></param>
+/// <param name="volume"></param>
+int AudioEngine::PlaySound(const string& soundName, const Vector2D& pos, float volume)
+{
+	int channelID = pInit->m_ChannelID++;
+	auto foundSound = pInit->m_Sounds.find(soundName);
+	if (foundSound == pInit->m_Sounds.end())
+	{
+		LoadSound(soundName);
+		foundSound = pInit->m_Sounds.find(soundName);
+		if (foundSound == pInit->m_Sounds.end())
+		{
+			return channelID;
+		}
+	}
+
+	FMOD::Channel* pChannel = nullptr;
+	pInit->pSystem->playSound(foundSound->second, nullptr, true, &pChannel);
+
+	if (pChannel)
+	{
+		FMOD_MODE currentMode;
+		foundSound->second->getMode(&currentMode);
+		FMOD_VECTOR position = FmodVector(pos);
+		pChannel->setVolume(volume);
+		pChannel->setPaused(false);
+		pInit->m_Channels[channelID] = pChannel;
+			
+	}
+
+	return channelID;
+
+}
+
+/// <summary>
+/// Setting Volume
+/// </summary>
+/// <param name="channelID"></param>
+/// <param name="volume"></param>
+void AudioEngine::SetVolume(int channelID, float volume)
+{
+	auto foundSound = pInit->m_Channels.find(channelID);
+	if (foundSound == pInit->m_Channels.end())
+	{
+		return;
+	}
+
+	foundSound->second->setVolume(volume);
+}
+
+//Creating Events
+
+/// <summary>
+/// Loading banks, banks stores all the sound and information for each event
+/// </summary>
+/// <param name="bankName"></param>
+/// <param name="flags"></param>
+void AudioEngine::LoadBank(const std::string& bankName, FMOD_STUDIO_LOAD_BANK_FLAGS flags)
+{
+	auto foundSound = pInit->m_Banks.find(bankName);
+	if (foundSound != pInit->m_Banks.end())
+	{
+		return;
+	}
+	FMOD::Studio::Bank* pBank;
+	pInit->pStudioSystem->loadBankFile(bankName.c_str(), flags, &pBank);
+	if (pBank)
+	{
+		pInit->m_Banks[bankName] = pBank;
+	}
+
+}
+
+/// <summary>
+/// Each event store in a bank, Load Events in two parts Event Description and EventInstance
+/// Description = information
+/// Instance = plays the sound
+/// </summary>
+/// <param name="eventName"></param>
+void AudioEngine::LoadEvent(const std::string& eventName)
+{
+	auto foundSound = pInit->m_Events.find(eventName);
+	if (foundSound != pInit->m_Events.end())
+	{
+		return;
+	}
+	FMOD::Studio::EventDescription* pEvDescription = NULL;
+	pInit->pStudioSystem->getEvent(eventName.c_str(), &pEvDescription);
+	if (pEvDescription)
+	{
+		FMOD::Studio::EventInstance* pEvInstance = NULL;
+		pEvDescription->createInstance(&pEvInstance);
+		if (pEvInstance)
+		{
+			pInit->m_Events[eventName] = pEvInstance;
+		}
+	}
+}
+
+/// <summary>
+/// Plays event, also checks if it was loaded, if not load it
+/// </summary>
+/// <param name="eventName"></param>
+void AudioEngine::PlayEvent(const string& eventName)
+{
+	auto foundSound = pInit->m_Events.find(eventName);
+	if (foundSound == pInit->m_Events.end())
+	{
+		LoadEvent(eventName);
+		foundSound = pInit->m_Events.find(eventName);
+		if (foundSound == pInit->m_Events.end())
+		{
+			return;
+		}
+	}
+	foundSound->second->start();
+}
+
+
+/// <summary>
+/// Check if event is playing 
+/// </summary>
+/// <param name="eventName"></param>
+/// <returns></returns>
+bool AudioEngine::EventPlaying(const string& eventName)
+{
+	auto foundSound = pInit->m_Events.find(eventName);
+	if (foundSound == pInit->m_Events.end())
+		return false;
+
+	FMOD_STUDIO_PLAYBACK_STATE* pstate = NULL;
+	if (foundSound->second->getPlaybackState(pstate) == FMOD_STUDIO_PLAYBACK_PLAYING)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+//Maths functions that help 
+FMOD_VECTOR AudioEngine::FmodVector(const Vector2D& pos)
+{
+	FMOD_VECTOR fVector;
+	fVector.x = pos.x;
+	fVector.y = pos.y;
+
+	return fVector;
+}
+
+float AudioEngine::DBtoVolume(float db)
+{
+	return powf(10.0f, 0.05f * db);
+}
+
+float AudioEngine::VolumetoDB(float volume)
+{
+	return 20.0f * log10f(volume);
+}
+
+
--- StudioSim/src/AudioEngine.h
+++ StudioSim/src/AudioEngine.h
@@ -2,20 +2,23 @@
 #include <fmod.h>
 #include <fmod.hpp>
 #include <fmod_studio.hpp>
+#include <string>
+#include <iostream>
+#include <map>
 #include "BasicIncludes.h"
 
+using namespace std;
 /// <summary>
 /// Vector used to position the sound in the game
-/// </summary>
 struct Vector2D
 {
 	float x;
 	float y;
 };
+/// </summary>
 
 /// <summary>
-/// Responsible for calling FMOD API 
-/// </summary>
+/// Responsible for the initialization FMOD API 
 struct Init
 {
 	Init();
@@ -23,13 +26,60 @@ struct Init
 
 	void InitUpdate();
 
+	int m_ChannelID;
+
+	/// <summary>
+	/// Maps with file anme, key is the even or sound
+	typedef map<string, FMOD::Sound*> SoundMap;
+	typedef map<int, FMOD::Channel*> ChannelMap;
+	typedef map<string, FMOD::Studio::EventInstance*> EventMap;
+	typedef map<string, FMOD::Studio::Bank*> BankMap;
+	/// </summary>
+
+	SoundMap m_Sounds;
+	ChannelMap m_Channels;
+	EventMap m_Events;
+	BankMap m_Banks;
+
+	/// <summary>
+	/// Fmod system and result
 	FMOD::System* pSystem;
+	FMOD::Studio::System* pStudioSystem;
 	FMOD_RESULT m_Result;
+	/// </summary>
 };
+/// </summary>
 
 class AudioEngine
 {
-	AudioEngine();
+public:
+	/// <summary>
+	/// Audio Engine Setup and controll
+	static void InitializingAudioEngine();
+	static void UpdateAudioEngine();
+	static void ShutDownAudioEngine();
+	/// </summary>
+
+	
+	void LoadSound(const string& soundName, bool looping = false, bool is2D = false);
+	void LoadEvent(const string& eventName);
+	void LoadBank(const string& bankName, FMOD_STUDIO_LOAD_BANK_FLAGS flags);
+	void UnloadSound(const string& soundName);
+	void PlayEvent(const string& eventName);
+	void SetVolume(int channelID, float volume);
+	void GetEventPar(const string& eventName, const string& parameterName, float pParameter);
+	void SetEventPar(const string& eventName, const string& parameterName, float value);
+	
+	
+	int PlaySound(const string& soundName, 
+		const Vector2D& pos = Vector2D{ 0,0 }, float volume = 0.0f);
+
+	bool EventPlaying(const string& eventName);
+
+	float DBtoVolume(float db);
+	float VolumetoDB(float volume);
+
+	FMOD_VECTOR FmodVector(const Vector2D& pos);
 	
 };
 
