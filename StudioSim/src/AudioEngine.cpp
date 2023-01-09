01d3b0107fb5646cdbdf92e51ce99122a8bf079c
Fernandes <71331946+Felp0@users.noreply.github.com>
2023-01-09 12:53:36 +0000

    Start of audio engine

--- StudioSim/src/AudioEngine.cpp
+++ StudioSim/src/AudioEngine.cpp
@@ -1,26 +1,96 @@
 #include "AudioEngine.h"
 
+//FMOD construction 
+
+//Calculation Functions
+
+/// <summary>
+/// Convert DB to linear Volume
+/// </summary>
+/// <param name="DB"> -> Logarithmic unit used to measure sound level </param>
+/// <returns> Returns the value of X to the power of Y </returns>
+float AudioEngine::ChangingDBToVolume(float DB)
+{
+	return powf(10.0/*X*/, 0.05f * DB/*Y*/);
+}
+
+
+/// <summary>
+/// Convert Linear Volume to DB
+/// </summary>
+/// <param name="volume"></param>
+/// <returns> Returns the logarith to the base 10 of x </returns>
+float AudioEngine::ChangingVolumeToDB(float volume)
+{
+	return 20.0f * log10f(volume);
+}
+
+/// <summary>
+/// Converting Vector2D struct to FMOD's Vector2
+/// </summary>
+/// <param name="pos"> ->Position of the 2D sound</param>
+/// <returns> Returns a FMOD Vector </returns>
+FMOD_VECTOR AudioEngine::ChangingVectorToFmodVector(const Vector2D& pos)
+{
+	FMOD_VECTOR fVector;
+	fVector.x = pos.x;
+	fVector.y = pos.y;
+
+	return fVector;
+}
+
+
+/// <summary>
+/// Creating FMOD studio system, Initializing FMOD studio System, Getting Low Level core system
+/// Error checking 
+/// </summary>
 Init::Init()
 {
-	pSystem = nullptr;
+	pSystem = NULL;
+	pStudioSystem = NULL;
 
-	m_Result = FMOD::System_Create(&pSystem);
-
-	//Error Check
+	//Creates core system
+	m_Result = FMOD::Studio::System::create(&pStudioSystem);
 	if (m_Result != FMOD_OK)
 	{
-		printf("Error creating the main system FMOD! (%d)\n", m_Result);
+		std::cout << "ERROR CREATING STUDIO SYSTEM: " << m_Result << endl;
 		exit(-1);
 	}
 
-	//Fmod Initialization
-	m_Result = pSystem->init(512, FMOD_INIT_NORMAL, 0); 
-
+	//Initialize Studio System
+	m_Result = pStudioSystem->initialize(64, FMOD_STUDIO_INIT_LIVEUPDATE, FMOD_INIT_PROFILE_ENABLE, NULL);
 	if (m_Result != FMOD_OK)
 	{
-		printf("Error initializing FMOD! (%d)", m_Result);
+		std::cout << "ERROR INITIALIZING STUDIO SYSTEM: " << m_Result << endl;
+		exit(-1);
+	}
+
+	//Gets core system
+	m_Result = pStudioSystem->getCoreSystem(&pSystem);
+	if (m_Result != FMOD_OK)
+	{
+		std::cout << "ERROR GETTING CORE SYSTEM: " << m_Result << endl;
 		exit(-1);
 	}
 
 
 }
+
+/// <summary>
+/// Deleting and releasing memory
+/// </summary>
+Init::~Init()
+{
+	pStudioSystem->unloadAll();
+	pStudioSystem->release();
+}
+
+/// <summary>
+/// Update Function for the initialization
+/// </summary>
+void Init::InitUpdate()
+{
+
+}
+
+//AudioEngine
--- StudioSim/src/AudioEngine.h
+++ StudioSim/src/AudioEngine.h
@@ -2,7 +2,13 @@
 #include <fmod.h>
 #include <fmod.hpp>
 #include <fmod_studio.hpp>
+#include <map>
+#include <string>
+#include <vector>
+#include <math.h>
 #include "BasicIncludes.h"
+
+using namespace std;
 
 /// <summary>
 /// Vector used to position the sound in the game
@@ -21,15 +27,56 @@ struct Init
 	Init();
 	~Init();
 
+	//Maps
+	typedef map<string, FMOD::Sound*> pSoundMap;
+	typedef map<int, FMOD::Channel*> pChannelMap;
+
 	void InitUpdate();
 
+	//System, Studio System and Result
 	FMOD::System* pSystem;
+	FMOD::Studio::System* pStudioSystem;
 	FMOD_RESULT m_Result;
+
+	//Channels
+	int ChannelID;
+
+	//Calls to map
+	pSoundMap m_SoundMap;
+	//pBankMap m_BankMap;
+	//pEventMap m_EventMap;
+	pChannelMap m_ChannelsMap;
+
 };
 
 class AudioEngine
 {
-	AudioEngine();
+public:
+	void Start();
+	void Update();
+	void Stop();
+
+	//Variables
 	
+	//Loading Functions
+	void LoadSound(const string& soundName, bool isloop, bool is3D);
+	void UnloadingSound(const string& soundName);
+
+	//Playing Functions
+	void PlaySound(const string& soundName, const Vector2D& pos = Vector2D{ 0,0 }, float DBVolume = 0.0f);
+	
+	bool IsSoundPlaying(const string& soundName);
+	
+	//Volume Control
+	void SetChannelVolume(int channelID, float volume);
+
+	//Stop Functions
+	void StopChannel(int channelID);
+
+	//Calculation Functions
+	float ChangingDBToVolume(float DB);
+	float ChangingVolumeToDB(float volume);
+
+	FMOD_VECTOR ChangingVectorToFmodVector(const Vector2D& pos);
 };
 
