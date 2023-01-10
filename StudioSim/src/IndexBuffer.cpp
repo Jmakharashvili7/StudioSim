32032dbfd70a7dad5c8526d75254ae8758fe0d03
Alexandru Ionescu <71647115+IonescuAlexandruM@users.noreply.github.com>
2023-01-10 09:57:49 +0000

    Revert "Merge remote-tracking branch 'origin/Lewis' into Alex"
    This reverts commit 517c1a07ab73b4edb1ea138564d09e7b58502afe, reversing
    changes made to 9b086dba02b4f10618718385f7346462624093cd.

--- StudioSim/src/IndexBuffer.cpp
+++ StudioSim/src/IndexBuffer.cpp
@@ -1,11 +1,10 @@
 #include "IndexBuffer.h"
 
-IndexBuffer::IndexBuffer(const unsigned int* data, unsigned int count)
+IndexBuffer::IndexBuffer(const unsigned int* data, unsigned int count) : m_count(0)
 {
 	GLCall(glGenBuffers(1, &m_rendererID));
 	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_rendererID));
 	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW));
-	m_count = count;
 }
 
 IndexBuffer::~IndexBuffer()
--- StudioSim/src/Renderer.h
+++ StudioSim/src/Renderer.h
@@ -1,12 +1,8 @@
 #pragma once
-#include "VertexArray.h"
-#include "IndexBuffer.h"
-#include "Shader.h"
 
-class Renderer
-{
-public:
-	void Clear();
-	void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader);
-};
+//class Renderer
+//{
+//public:
+//	static void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+//};
 
