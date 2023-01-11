06e91b9260490b1bb9bc7b36a8415565fe2e17cf
Fernandes <71331946+Felp0@users.noreply.github.com>
2023-01-11 20:03:59 +0000

    Audio Engine Re-do Simple to use now, need more work

---
+++ StudioSim/AudioEngine2D.h
@@ -0,0 +1,85 @@
+#pragma once
+#ifndef AUDIO_ENGINE_2D
+#define AUDIO_ENGINE_2D
+
+#include "fmod_studio.hpp"
+#include "fmod.hpp"
+#include "fmod_errors.h"
+#include <string>
+#include <vector>
+#include <math.h>
+#include <iostream>
+#include <map>
+
+#endif
+using namespace std;
+
+/// <summary>
+/// Struct For FMOD Implementation, used to separete from audio engine, to prevent weird bugs
+/// </summary>
+struct FmodImplementation
+{
+	FmodImplementation();
+	~FmodImplementation();
+
+	//Maps
+	typedef map<string, FMOD::Sound*> SoundMap;
+	typedef map<int, FMOD::Channel*> ChannelMap;
+
+	//Functions
+	void Update();
+
+	//Pointers and variables
+	FMOD::System* pFmodSystem;
+
+	SoundMap	m_SoundMap;
+	ChannelMap	m_ChannelMap;
+
+	int			m_NextChannelID;
+};
+
+
+/// <summary>
+/// 2D Audio Engine Class
+/// </summary>
+class AudioEngine2D
+{
+public:
+
+	static void Initialization();
+	static void Update();
+	static void Shutdown();
+
+	/// <summary>
+	/// Function used to load sounds
+	/// </summary>
+	/// <param name="pathToSound"> ->Path to Sound e.g "StudioSim/Sounds/Quack.wav </param>
+	/// <param name="isloop"> ->Is the sound going to loop ? used here for the FMOD_MODE</param>
+	/// <param name="isStream"> ->Set to true if using a larger file</param>
+	void LoadSound(const string& pathToSound, bool isloop, bool isStream);
+
+	/// <summary>
+	/// Function used to play sound
+	/// </summary>
+	/// <param name="pathToSound"> ->Path to Sound e.g "StudioSim/Sounds/Quack.wav</param>
+	/// <param name="isloop"> ->Should the sound loop when it ends ?</param>
+	/// <param name="isStream"> ->Set to true if using a larger file</param>
+	/// <param name="volumeDB"> ->Volume of the sound</param>
+	/// <returns></returns>
+	int PlaySound(const string& pathToSound, bool isloop, bool isStream, float volumeDB);
+
+
+	/// <summary>
+	/// Changes DB to volume
+	/// </summary>
+	/// <param name="DB"></param>
+	/// <returns></returns>
+	float ChangingDBToVolume(float DB);
+
+
+};
+
+//https://ebookcentral.proquest.com/lib/staffordshire/reader.action?docID=4717687&query=Getting+Started+with+C%2B%2B+Audio+Programming+for+Game+Development#
+//https://www.programmer-books.com/wp-content/uploads/2018/08/Game-Programming-in-C-Creating-3D-Games.pdf
+//https://www.fmod.com/docs/2.02/api/white-papers-getting-started.html
+//https://www.codyclaborn.me/tutorials/making-a-basic-fmod-audio-engine-in-c/
--- StudioSim/src/Application.cpp
+++ StudioSim/src/Application.cpp
@@ -19,90 +19,90 @@ int main(void)
    Quack* quackEngine = new Quack();
    quackEngine->InitEngine();
 
-    ///* Initialize the GLFW library */
-    //if (!glfwInit())
-    //    return -1;
+    /* Initialize the GLFW library */
+    if (!glfwInit())
+        return -1;
 
-    //GLFWwindow* window;
+    GLFWwindow* window;
 
-    ///* Create a windowed mode window and its OpenGL context */
-    //window = glfwCreateWindow(1280, 800, "Studio Sim", NULL, NULL); 
-    //if (!window)
-    //{
-    //    glfwTerminate();
-    //    return -1;
-    //}
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1280, 800, "Studio Sim", NULL, NULL); 
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
-    ///* Make the window's context current */
-    //glfwMakeContextCurrent(window);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
-    ///* Initialize the Glew Library*/
-    //glewExperimental = GL_TRUE;
-    //glewInit();
+    /* Initialize the Glew Library*/
+    glewExperimental = GL_TRUE;
+    glewInit();
 
-    //float positions[] = {
-    //-0.5f, -0.5f, // 0
-    // 0.5f, -0.5f, // 1
-    // 0.5f,  0.5f, // 2
-    //-0.5f,  0.5f, // 3
-    //};
-    //
-    //unsigned int indices[] = {
-    //    0, 1, 2,
-    //    2, 3, 0
-    //};
-    //
-    //GLuint vertexbuffer;
-    //glGenBuffers(1, &(vertexbuffer));
-    //glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    //
-    //unsigned int vao;
-    //glGenVertexArrays(1, &vao);
-    //glBindVertexArray(vao);
-    //
-    //VertexArray va;
-    //VertexBuffer vb(positions, 4 * 2 * sizeof(float));
-    //
-    //VertexBufferLayout layout;
-    //layout.Push<float>(2);
-    //va.AddBuffer(vb, layout);
-    //
-    //IndexBuffer ib(indices, 6);
-    //
-    //Shader shader("shaders/basic.shader");
-    //shader.Bind();
-    //shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+    float positions[] = {
+    -0.5f, -0.5f, // 0
+     0.5f, -0.5f, // 1
+     0.5f,  0.5f, // 2
+    -0.5f,  0.5f, // 3
+    };
+    
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+    
+    GLuint vertexbuffer;
+    glGenBuffers(1, &(vertexbuffer));
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
+    VertexArray va;
+    VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+    
+    VertexBufferLayout layout;
+    layout.Push<float>(2);
+    va.AddBuffer(vb, layout);
+    
+    IndexBuffer ib(indices, 6);
+    
+    Shader shader("shaders/basic.shader");
+    shader.Bind();
+    shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 
-    //va.Unbind();
-    //vb.Unbind();
-    //ib.Unbind();
-    //shader.Unbind();
+    va.Unbind();
+    vb.Unbind();
+    ib.Unbind();
+    shader.Unbind();
 
-    //GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-    //GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
 
-    ///* Loop until the user closes the window */
-    //while (!glfwWindowShouldClose(window))
-    //{
-    //    /* Render here */
-    //    GLCall(glClear(GL_COLOR_BUFFER_BIT));
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-    //    QE_INFO("Testing!");
+        QE_INFO("Testing!");
 
-    //    ////shader.Bind();
-    //    va.Bind();
-    //    ib.Bind();  
-    //    vb.Bind();
-    //    shader.Bind();
-    //    
-    //    GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+        ////shader.Bind();
+        va.Bind();
+        ib.Bind();  
+        vb.Bind();
+        shader.Bind();
+        
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
-    //    glfwSwapBuffers(window);
+        glfwSwapBuffers(window);
 
-    //    /* Poll for and process events */
-    //    glfwPollEvents();
-    //}
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
 
     
     glfwTerminate();
