f450cc1873a08342e84f2a1df064de7809f0e531
Spike Owen <72083521+spikeowen@users.noreply.github.com>
2023-01-16 11:43:43 +0000

    1 Comment and extra line

--- StudioSim/src/Quack.cpp
+++ StudioSim/src/Quack.cpp
@@ -90,8 +90,10 @@ int Quack::InitEngine()
 		cout << "FrameBuffer Not Finished." << endl;
 
 	
-
+	//Adding depth buffer to framebuffer
 	glBindFramebuffer(GL_FRAMEBUFFER, depthFBO);
+	//Buffer, attachments, texture type,the texture, mipmap level
+	//glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthTexture.depthMap, 0);
 	glDrawBuffer(GL_NONE);
 	glReadBuffer(GL_NONE);
