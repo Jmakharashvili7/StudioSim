410cb6fd6b23f0f426794f57c2c02286ebc7ee7f
Lewis Heath <h012549j@student.staffs.ac.uk>
2023-01-18 16:54:16 +0000

    Tidy structs up

--- StudioSim/src/QuackDataTypes.h
+++ StudioSim/src/QuackDataTypes.h
@@ -3,12 +3,14 @@
 
 struct TextureData
 {
-	TextureData(const std::string& inTexturePath) : texturePath(inTexturePath),
-		internalFormat(GL_RGB), imageFormat(GL_RGB) {};
-
 	TextureData(const std::string& inTexturePath, const GLint inInternalFormat, const GLint inImageFormat)
 		: texturePath(inTexturePath), internalFormat(inInternalFormat),
 		imageFormat(inImageFormat) {};
+
+	TextureData(const std::string& inTexturePath) : texturePath(inTexturePath),
+		internalFormat(GL_RGB), imageFormat(GL_RGB) {};
+
+	TextureData() : texturePath(""), internalFormat(0), imageFormat(0) {};
 
 	std::string texturePath;
 	GLint internalFormat;
@@ -20,6 +22,8 @@ struct PhysicsData
 	PhysicsData(const bool inSimulateGravity, const float inMass, const float inJumpHeight)
 		: bsimulateGravity(inSimulateGravity), mass(inMass), jumpHeight(inJumpHeight) {};
 
+	PhysicsData() : bsimulateGravity(false), mass(0.0f), jumpHeight(0.0f) {};
+
 	bool bsimulateGravity;
 	float mass;
 	float jumpHeight;
@@ -30,7 +34,7 @@ struct AnimationData
 	AnimationData(const bool inAnimated, const int inRows, const int inColumns)
 		: banimated(inAnimated), rows(inRows), columns(inColumns) {};
 
-	AnimationData() : banimated(false){};
+	AnimationData() : banimated(false), rows(0), columns(0) {};
 
 	bool banimated;
 	int rows;
