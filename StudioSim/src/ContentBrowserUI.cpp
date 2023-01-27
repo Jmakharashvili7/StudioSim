e4de31d54bb98acffbbc87d7aa804883baa8ab60
Aki Wainwright <72081159+akiwainwright@users.noreply.github.com>
2023-01-27 14:03:00 +0000

    added content browser

--- StudioSim/src/ContentBrowserUI.cpp
+++ StudioSim/src/ContentBrowserUI.cpp
@@ -1,4 +1,8 @@
 #include "ContentBrowserUI.h"
+#include <filesystem>
+#include <string>
+
+using namespace std::filesystem;
 
 ContentBrowserUI::ContentBrowserUI(std::string name) : UIWindow(name)
 {
@@ -12,16 +16,18 @@ ContentBrowserUI::~ContentBrowserUI()
 void ContentBrowserUI::Render()
 {
 	//name of folder to use for content browser
-	std::string contentBrowserDirectory = "res";
+	const std::string rootBrowserDirectory = "res";
 
 	ImGui::Begin("Content Browser");
 
 	//loop through the directory in contentBrowser and show all directories
-	for (auto& directory : std::filesystem::directory_iterator(contentBrowserDirectory))
+	//directory iterator goes through all subdirectories within the directory passed in
+	//directory entry is each directory where the directory iterator is located
+	for (directory_entry directory : directory_iterator(rootBrowserDirectory))
 	{
-		ImGui::Text("%s", directory.path().c_str());
+		std::string path = directory.path().string();
+		ImGui::Text("%s", path.c_str());
 	}
-
 	ImGui::End();
 }
 
