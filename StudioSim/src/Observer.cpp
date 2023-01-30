1fc608e6fa87df74c97f95db799356371bfdb9f2
Adeogo Adeniji <68272077+Adeogo1@users.noreply.github.com>
2023-01-30 11:52:06 +0000

    ntd

--- StudioSim/src/Observer.cpp
+++ StudioSim/src/Observer.cpp
@@ -1,7 +1,7 @@
 #include "Observer.h"
 #include "ExampleSingleton.h"
-
-Observer::Observer()
-{
-	ExampleSingleton::Instance()->Test();
-}
+//
+//Observer::Observer()
+//{
+//	ExampleSingleton::Instance()->Test();
+//}
