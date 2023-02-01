afa00818ea0ac8960f8702f21eeba41770c7ff94
Aki Wainwright <72081159+akiwainwright@users.noreply.github.com>
2023-02-02 05:24:48 +0900

    Revert "fixed input fuctions"
    This reverts commit ec2ba50bc627050b070205b8762524df600e428b.

--- StudioSim/src/Quack.cpp
+++ StudioSim/src/Quack.cpp
@@ -153,8 +153,20 @@ Texture* Quack::GetTexture(std::string textureName)
 
 void Quack::HandleInput()
 {
-	KeyEvent key = KeyboardClass::ReadKey();
-	MouseEvent e = MouseClass::ReadEvent();
+	KeyEvent key;
+	MouseEvent e;
+
+	//Keyboard Input
+	if (!KeyboardClass::KeyBufferIsEmpty())
+	{
+		key = KeyboardClass::ReadKey();
+	}
+
+	//Mouse Input
+	if (!MouseClass::IsEventBufferEmpty())
+	{
+		e = MouseClass::ReadEvent();
+	}
 
 	for(UIWindow* window : m_uiMain->GetAllWindows())
 	{
