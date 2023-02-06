3d563a4cb5abb15609c7deb667206ed76d477260
Adeogo Adeniji <68272077+Adeogo1@users.noreply.github.com>
2023-02-06 13:06:39 +0000

    input + collision resolution

--- StudioSim/src/PhysicsComponent.cpp
+++ StudioSim/src/PhysicsComponent.cpp
@@ -1,9 +1,10 @@
 #include "pch.h"
 
 #include "PhysicsComponent.h"
+#include "CollisionManager.h"
 
 PhysicsComponent::PhysicsComponent(Actor* owner, int updateOrder, const float mass, const bool bSimulateGravity, const float gravityMultiplier) : Component(owner, updateOrder), m_InverseMass(1 / mass)
-{	
+{
 	m_Gravity = Vector3(0, -m_GravitationalValue, 0);
 	m_Acceleration = m_InverseMass * (1 / m_InverseMass) * m_Gravity;
 	m_Dampening = 0.995f;
@@ -13,6 +14,8 @@ PhysicsComponent::PhysicsComponent(Actor* owner, int updateOrder, const float ma
 	m_updateOrder = updateOrder;
 	SetSimulateGravity(bSimulateGravity);
 	SetGravityValue(gravityMultiplier);
+	m_Contacts = new CollisionResolver();
+
 }
 
 
@@ -23,17 +26,23 @@ PhysicsComponent::~PhysicsComponent()
 
 void PhysicsComponent::Update(float deltaTime)
 {
-	if (m_InverseMass <= 0.0f || !m_bSimulateGravity || m_bOnGround) return;
+	if (m_InverseMass <= 0.0f || !m_bSimulateGravity) return;
 
 	m_owningActor->AdjustPosition(m_Velocity * deltaTime);
-	
+
 	Vector3 resultingAcc = m_Acceleration;
 	resultingAcc += m_Force * m_InverseMass;
 
 	m_Velocity += resultingAcc * deltaTime;
 
 	m_Velocity *= powf(m_Dampening, deltaTime);
-	
+
+	if (m_bOnGround)
+	{
+		m_Contacts->Resolve(deltaTime);
+	}
+
+
 	ClearAccumulator();
 }
 
@@ -50,7 +59,11 @@ void PhysicsComponent::ClearAccumulator()
 
 void PhysicsComponent::AddForce(const Vector3& force)
 {
-	SetOnGround(false);
+	cout << m_Force.x << "  " << m_Force.y << "  " << m_Force.z << endl;
+	if (m_bOnGround)
+	{
+		m_bOnGround = !m_bOnGround;
+	}
 	m_Force += force;
 }
 
@@ -60,11 +73,14 @@ void PhysicsComponent::SetOnGround(const bool bOnGround)
 
 	if (bOnGround)
 	{
-		ResetForces();
+		//ResetForces();
+		m_Contacts->m_PhysicsObject[0] = this;
+		m_Contacts->m_PhysicsObject[1] = nullptr;
+
 	}
 	else
 	{
-		UpdateAccelerationByGravity();
+		//UpdateAccelerationByGravity();
 	}
 }
 
@@ -84,6 +100,6 @@ void PhysicsComponent::SetSimulateGravity(const bool bSimulateGravity)
 
 void PhysicsComponent::ResetForces()
 {
-	SetAcceleration(Vector3(0, 0, 0));
+	//SetAcceleration(Vector3(0, 0, 0));
 	SetVelocity(Vector3(0, 0, 0));
 }
