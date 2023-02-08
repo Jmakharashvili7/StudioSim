2920aecacc85eef61fc9ceb6461f7ee013518133
Fernandes <71331946+Felp0@users.noreply.github.com>
2023-02-08 17:38:38 +0000

    I want to fuck lew

--- StudioSim/src/Scene.cpp
+++ StudioSim/src/Scene.cpp
@@ -41,7 +41,7 @@ void Scene::RenderScene()
 {
 	m_frameBuffer->Bind();
 	/* Render here */
-	glClearColor(m_uiMain->GetColor().x, m_uiMain->GetColor().y, m_uiMain->GetColor().z, 1.0f);
+	glClearColor(12.f/255.0f, 19.0f/255.0f, 25.0f/255.f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	Render();
